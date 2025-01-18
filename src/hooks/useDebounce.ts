020611370cb3f83d9306ce2514b67a2fd3b81afb
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2025-01-18 01:56:57 -0300

    feat: create debounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+export function useDebounce<T>(value: T, delay: number = 500) {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+
+  return debounceValue
+}
