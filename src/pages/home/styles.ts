4e5c12d1861052642b5637b251b200c0649eb6ff
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2025-01-18 02:56:23 -0300

    fix: container width calculation

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.main`
-  max-width: calc(864px + 1.5rem);
+  max-width: calc(864px + 3rem);
   width: 100%;
   padding: 0 1.5rem;
   margin: -5.5rem auto 1.5rem;
