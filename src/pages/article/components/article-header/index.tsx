2a75430e5a46fe5c17a9462081209b4bc8daa459
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2025-01-19 22:16:13 -0300

    feat: create article header component

---
+++ src/pages/article/components/article-header/index.tsx
@@ -0,0 +1,55 @@
+import { LinkButton } from '@/components/ui'
+
+import {
+  FaChevronLeft,
+  FaGithub,
+  FaCalendarDay,
+  FaComment,
+} from 'react-icons/fa'
+import { TbExternalLink } from 'react-icons/tb'
+import {
+  Container,
+  Content,
+  LinkGroup,
+  SocialMediaGroup,
+  SocialMedia,
+} from './styles'
+
+export function ArticleHeader() {
+  return (
+    <Container>
+      <Content>
+        <LinkGroup>
+          <LinkButton>
+            <FaChevronLeft size={18} />
+            BACK
+          </LinkButton>
+          <LinkButton target="_blank">
+            GO TO GITHUB <TbExternalLink size={18} />
+          </LinkButton>
+        </LinkGroup>
+
+        <h1>
+          Expose refs on DOM Components as Direct Handles to the actual DOM Node
+        </h1>
+
+        <SocialMediaGroup>
+          <SocialMedia>
+            <FaGithub size={18} />
+            <span>guizioliveira</span>
+          </SocialMedia>
+
+          <SocialMedia>
+            <FaCalendarDay size={18} />
+            <span>2 weeks ago</span>
+          </SocialMedia>
+
+          <SocialMedia>
+            <FaComment size={18} />
+            <span>5 comments</span>
+          </SocialMedia>
+        </SocialMediaGroup>
+      </Content>
+    </Container>
+  )
+}
---
+++ src/pages/article/components/article-header/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  padding: 2rem;
+
+  background: ${({ theme }) => theme.colors.base.profile};
+  border-radius: 10px;
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  h1 {
+    font: ${({ theme }) => theme.font.title.l};
+    color: ${({ theme }) => theme.colors.base.title};
+  }
+`
+
+export const SocialMediaGroup = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    gap: 1rem;
+  }
+`
+
+export const SocialMedia = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  color: ${({ theme }) => theme.colors.base.span};
+
+  svg {
+    color: ${({ theme }) => theme.colors.base.label};
+  }
+
+  span {
+    word-wrap: break-word;
+  }
+
+  @media screen and (max-width: 768px) {
+    font: ${({ theme }) => theme.font.text.xs};
+  }
+`
+
+export const LinkGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 0.75rem;
+`
